5a1756ed53e5cbf762fc5aed0cb9e3928f27cfda
srilathadinakar <srilathadinakar@gmail.com>
2024-09-25 00:33:40 +0530

    updated

--- script.js
+++ script.js
@@ -1,83 +1,59 @@
 // Getting all the HTML Elements
 const form = document.getElementById("form");
-const description = document.getElementById("desc");
-const amount = document.getElementById("amount");
-
-const category = document.getElementById("category");
-const type = category.value;
-const selected = document.getElementById("selected");
-
-const msg = document.getElementById("msg");
+const descriptionInput = document.getElementById("desc");
+const amountInput = document.getElementById("amount");
 
 const entries = document.getElementById("entries");
-const add = document.getElementById("add");
 
 const updIncome = document.getElementById("total-income");
 const updExpense = document.getElementById("total-expense");
 const updBalance = document.getElementById("net-balance");
 
-const table = document.getElementById("table");
-
-// Form Validation
-const formValidation = () =>{
-    if(description.value === "" || amount.value === ""){
-        msg.innerHTML = "Please Enter the Fields";
-    }
-    else{
-        msg.innerHTML = "";
-        getData();
-        add.setAttribute("data-modal-hide","static-modal");       
-        add.click();
-        (()=>{           
-            add.setAttribute("data-modal-hide","")          
-        })()
-    }
-}
+const data = JSON.parse(localStorage.getItem("data")) || [];
 
 form.addEventListener("submit",(e)=>{
     e.preventDefault();
-    formValidation();
-})
 
-// Getting data from form and store it in array of objects
-let data = [{}];
-const getData = () =>{
-    data.push({
-        category:category.value,
-        description:description.value,
-        amount:parseFloat(amount.value)
-    })
-    localStorage.setItem("data",JSON.stringify(data));   
+    const description = descriptionInput.value;
+    const amount = parseFloat(amountInput.value);
+    const type = document.querySelector('input[name="type"]:checked').value;       
+
+    data.push({description,amount,type});
+
+    resetForm();
+
+    localStorage.setItem("data",JSON.stringify(data));
+
     createTable(data);
     updateSummary();
-}
+})
 
 // getting data from getdata and create div in frontend
-const createTable = (data) =>{    
+ const createTable = (data) =>{    
     entries.innerHTML = "";   
     data.map((ele,y)=>{             
         return(
             entries.innerHTML +=`
-                <tr id="${y}" class="border border-b">
-                    <td class="p-3 w-1/6 text-lg pl-5">${ele.category}</td>
+                <tr id=${y} class="border border-b">                   
                     <td class="p-3 w-1/6 text-lg pl-5">${ele.description}</td>
-                    <td class="p-3 w-1/6 text-lg pl-5">${ele.amount}</td>                     
-                    <td><button data-id="${y}" onclick="editTask(this);" class="edit pl-10" data-modal-target="static-modal" data-modal-toggle="static-modal"><i class="material-symbols-rounded" style="color:green">Edit</i></button></td>
+                    <td class="p-3 w-1/6 text-lg pl-5">${ele.amount}</td>   
+                    <td class="p-3 w-1/6 text-lg pl-5">${ele.type}</td>                  
+                    <td><button class="edit pl-10" onclick="editTask(this)"><i class="material-symbols-rounded" style="color:green">Edit</i></button></td>
                     <td><button class="delete pl-10" onclick="deleteTask(this); createTable(data);"><i class="material-symbols-rounded" style="color:red">Delete</i></button></td>          
                 </tr>            
             `
         )       
     })
     resetForm();
-}
+} 
 
-const updateSummary = () => {
+ const updateSummary = () => {
     let totalIncome = data
-        .filter((e) => e.category === "Income")
+        .filter((e) => e.type === "Income")
         .reduce((sum, curr) => sum + curr.amount, 0);      
         
     let totalExpense = data
-        .filter((e) => e.category === "Expense")
+        .filter((e) => e.type === "Expense")
         .reduce((sum, curr) => sum + curr.amount, 0);
 
     updIncome.innerText = `₹${totalIncome}`;
@@ -89,10 +65,10 @@ document.querySelectorAll('input[name="filter"]').forEach(radio => {
     radio.addEventListener('change', (e) => {
         const filter = e.target.value;      
         let filteredEntries;
-        if(filter === 'Income') {
-            filteredEntries = data.filter((e) => e.category === "Income");                     
-        }else if (filter === 'Expense') {            
-            filteredEntries = data.filter((e) => e.category === "Expense");                
+        if(filter === 'TypeIncome') {
+            filteredEntries = data.filter((e) => e.type === "Income");                     
+        }else if (filter === 'TypeExpense') {            
+            filteredEntries = data.filter((e) => e.type === "Expense");                
         }else {
             filteredEntries = data;            
         }       
@@ -102,30 +78,26 @@ document.querySelectorAll('input[name="filter"]').forEach(radio => {
 
 // reset the form
 const resetForm = () =>{
-    category.value = `${selected.value}`; 
-    description.value = "";
-    amount.value = "";
+    descriptionInput.value = "";
+    amountInput.value = "";
 }
-
-(()=>{
-    data = JSON.parse(localStorage.getItem("data")) || [];
-    createTable(data);
-    updateSummary();
-})()
 
 //edit function
 const editTask = (e) =>{
     let result = e.parentElement.parentElement;
-    category.value = result.children[0].innerHTML;
-    description.value = result.children[1].innerHTML;
-    amount.value = result.children[2].innerHTML;
-
+    descriptionInput.value = result.children[0].innerHTML;
+    amountInput.value = result.children[1].innerHTML;
+    document.querySelector('input[name="type"]').value = result.children[2].innerHTML;    
+    
     deleteTask(e);   
-}
+} 
 
 const deleteTask = (e) =>{
     e.parentElement.parentElement.remove();
     data.splice(e.parentElement.parentElement.id,1);
     localStorage.setItem("data",JSON.stringify(data));
-    updateSummary()  
+    updateSummary();
 }
+
+createTable(data);
+updateSummary();
